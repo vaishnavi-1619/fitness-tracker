c3e73a4d0372fd3ab01d3f13c4472d7136edeb75
vaishnavi-1619 <vaishnavikolhe22@gmail.com>
2026-06-06 14:19:29 +0530

    added user

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,36 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role= UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+}
